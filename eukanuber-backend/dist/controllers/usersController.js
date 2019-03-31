a763308aad290e01ef987ae12d7d6ec0682daed4
Nano Vazquez <mariano.vazquez@mulesoft.com>
2019-03-31 12:34:46 -0300

    [backend] Remove dist files

--- eukanuber-backend/dist/controllers/usersController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getUsers(req, res, next) {
-  console.log("6");
-  res.send("pepesss");
-  next();
-}
-exports.default = { getUsers };
-//# sourceMappingURL=usersController.js.map
