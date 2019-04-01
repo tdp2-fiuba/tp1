0a146fd170938237ad153a815616768cbda1806b
Julián Scialabba <julian.scialabba@gmail.com>
2019-04-01 02:10:48 -0300

    Add menu to home activity

--- eukanuber-app/app/src/main/java/com/fi/uba/ar/tdp2/eukanuber/activity/HomeActivity.java
+++ eukanuber-app/app/src/main/java/com/fi/uba/ar/tdp2/eukanuber/activity/HomeActivity.java
@@ -2,7 +2,6 @@ package com.fi.uba.ar.tdp2.eukanuber.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 
 import com.fi.uba.ar.tdp2.eukanuber.R;
 import com.fi.uba.ar.tdp2.eukanuber.model.Post;
@@ -22,15 +21,15 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class HomeActivity extends MenuActivity
+        implements OnMapReadyCallback {
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        this.createMenu();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -40,16 +39,6 @@ public class HomeActivity extends FragmentActivity implements OnMapReadyCallback
         System.out.print("user is: "+ user);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
