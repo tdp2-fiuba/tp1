d07ad9eb97742fddaa7bd8a2f495ff0713091a04
Julián Scialabba <julian.scialabba@gmail.com>
2019-04-02 00:33:57 -0300

    Set user position on map home

--- eukanuber-app/app/src/main/java/com/fi/uba/ar/tdp2/eukanuber/activity/HomeActivity.java
+++ eukanuber-app/app/src/main/java/com/fi/uba/ar/tdp2/eukanuber/activity/HomeActivity.java
@@ -1,7 +1,16 @@
 package com.fi.uba.ar.tdp2.eukanuber.activity;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import com.fi.uba.ar.tdp2.eukanuber.R;
 import com.fi.uba.ar.tdp2.eukanuber.model.Post;
@@ -24,6 +33,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class HomeActivity extends MenuActivity
         implements OnMapReadyCallback {
     private GoogleMap mMap;
+    private final int PERMISSION_FINE_LOCATION = 1;
+    private LocationListener locationListener;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,20 +46,85 @@ public class HomeActivity extends MenuActivity
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+
         Intent intent = getIntent();
         String user = intent.getStringExtra("user");
-        System.out.print("user is: "+ user);
+        System.out.print("user is: " + user);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+        this.checkPermissionsLocation();
+    }
+
+    private void checkPermissionsLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSION_FINE_LOCATION);
+            return;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+                    updateMapLocation();
+                } else {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "Sin permisos necesarios para utilizar la aplicacion",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+                return;
+            }
+        }
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        updateMapLocation();
         getPosts();
+    }
+
+    private void updateMapLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        LatLng currentPosition = new LatLng(location.getLatitude(), location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(currentPosition).title("Marker in current position"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentPosition));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f));
     }
 
     private void getPosts() {
@@ -56,7 +133,7 @@ public class HomeActivity extends MenuActivity
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         PostService postService = retrofit.create(PostService.class);
-        Call< List<Post> > call = postService.getPost();
+        Call<List<Post>> call = postService.getPost();
 
         call.enqueue(new Callback<List<Post>>() {
             @Override
