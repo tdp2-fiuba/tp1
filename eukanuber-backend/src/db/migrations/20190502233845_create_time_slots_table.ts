05466c9ec37696ea67a0d89eeb95ae421f89065f
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-05-03 00:59:10 -0300

    First approach to trip price calculation.
    (cherry picked from commit 9812527476fc8b6accf4d769bfc01668237a7736)

---
+++ eukanuber-backend/src/db/migrations/20190502233845_create_time_slots_table.ts
@@ -0,0 +1,28 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.createTable('timeSlots', tableBuilder => {
+    tableBuilder
+      .uuid('id')
+      .unique()
+      .notNullable()
+      .primary()
+      .defaultTo(knex.raw('uuid_generate_v4()'));
+
+    tableBuilder.integer('hourStart').notNullable();
+    tableBuilder
+      .integer('minStart')
+      .defaultTo(0)
+      .notNullable();
+    tableBuilder.integer('hourEnd').notNullable();
+    tableBuilder
+      .integer('minEnd')
+      .defaultTo(0)
+      .notNullable();
+    tableBuilder.float('price').defaultTo(1);
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.dropTableIfExists('timeSlots');
+}
