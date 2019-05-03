9812527476fc8b6accf4d769bfc01668237a7736
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-05-03 00:59:10 -0300

    First approach to trip price calculation.

--- eukanuber-backend/src/services/tripsService.ts
+++ eukanuber-backend/src/services/tripsService.ts
@@ -1,26 +1,26 @@
-import Knex from "knex";
-import db from "../db/db";
-import { ICreateTripData, ITrip, TripStatus } from "../models";
-import googleMapsService from "./googleMapsService";
+import Knex from 'knex';
+import db from '../db/db';
+import { ICreateTripData, ITrip, TripStatus } from '../models';
+import googleMapsService from './googleMapsService';
 
 async function getTrips(status: TripStatus) {
   const trips = await db
-    .table("trips")
-    .orderBy("createdDate", "desc")
+    .table('trips')
+    .orderBy('createdDate', 'desc')
     .modify((queryBuilder: Knex.QueryBuilder) => {
       if (status) {
-        queryBuilder.where("status", status);
+        queryBuilder.where('status', status);
       }
     })
     .select();
 
-  return trips.map((trip: any) => ({ ...trip, pets: trip.pets.split(",") }));
+  return trips.map((trip: any) => ({ ...trip, pets: trip.pets.split(',') }));
 }
 
 async function getTripById(id: string): Promise<ITrip> {
   const result = await db
-    .table("trips")
-    .where("id", id)
+    .table('trips')
+    .where('id', id)
     .select()
     .first();
 
@@ -30,7 +30,7 @@ async function getTripById(id: string): Promise<ITrip> {
 
   return {
     ...result,
-    pets: result.pets.split(",")
+    pets: result.pets.split(','),
   };
 }
 
@@ -39,35 +39,35 @@ async function createTrip(trip: ICreateTripData): Promise<ITrip> {
   const originCoordinates = await googleMapsService.getGeocode(trip.origin);
   const destinationCoordinates = await googleMapsService.getGeocode(trip.destination);
   const routes = await googleMapsService.getDirections(originCoordinates, destinationCoordinates);
-  const routeData = calculateRouteData(routes);
+  const routeData = await calculateTripData(routes, trip.pets);
 
   const newTrip = {
     ...trip,
-    pets: trip.pets.join(",").replace(/\s/g, ""),
+    pets: trip.pets.join(',').replace(/\s/g, ''),
     originCoordinates,
     destinationCoordinates,
-    clientId: "dummyClientId", // TODO: remove
+    clientId: 'dummyClientId', // TODO: remove
     status: TripStatus.PENDING,
     createdDate: new Date().toISOString(),
     ...routeData,
-    routes
+    routes,
   };
 
   const tripCreated = await db
     .insert(newTrip)
-    .into("trips")
-    .returning("*");
+    .into('trips')
+    .returning('*');
 
   return {
     ...tripCreated[0],
-    pets: trip.pets
+    pets: trip.pets,
   };
 }
 
 async function updateTripStatus(id: string, status: TripStatus) {
   await db
-    .table("trips")
-    .where("id", id)
+    .table('trips')
+    .where('id', id)
     .update({ status });
 
   return await this.getTripById(id);
@@ -81,23 +81,49 @@ async function assignDriverToTrip(id: string, driverId: string) {
   }
 
   await db
-    .table("trips")
-    .where("id", id)
+    .table('trips')
+    .where('id', id)
     .update({ driverId, status: TripStatus.DRIVER_GOING_ORIGIN });
 
   return await getTripById(id);
 }
 
-function calculateRouteData(routes: string) {
-  const distanceMultiplier = 0.2;
+async function calculateTripData(routes: string, pets: string[]) {
   const route = JSON.parse(routes)[0];
-  const price = route.legs[0].duration.value * distanceMultiplier;
+  const price = await computeTripCost(route.legs[0].distance.value, pets);
 
   return {
     distance: route.legs[0].distance.text,
     duration: route.legs[0].duration.text,
-    price: `$${price.toFixed(2)}`
+    price: `$${price.toFixed(2)}`,
   };
+}
+
+async function computeTripCost(distance: number, pets: string[]) {
+  const distanceMultiplier = 0.2;
+  const defaultTimeSlotPrice = 20;
+  const petSizeMultiplier: { [name: string]: number } = { S: 0.1, M: 0.3, L: 0.5 };
+
+  const now = Date.now();
+
+  var minutes = 1000 * 60;
+  var hours = minutes * 60;
+
+  let timeSlotCost: number = await db
+    .table('timeSlots')
+    .select('price')
+    .where('hourStart', '<=', Math.round(now / hours))
+    .andWhere('hourEnd', '>', Math.round(now / hours))
+    .andWhere('minStart', '<=', Math.round(now / minutes))
+    .andWhere('minEnd', '<=', Math.round(now / minutes));
+
+  if (timeSlotCost == undefined) {
+    timeSlotCost = defaultTimeSlotPrice;
+  }
+
+  let costPets: number = pets.map(petSize => petSizeMultiplier[petSize] * distance).reduce((p1, p2) => p1 + p2);
+
+  return distance * distanceMultiplier + costPets + timeSlotCost;
 }
 
 async function getRoute(origin: string, destination: string) {
@@ -112,5 +138,5 @@ export default {
   createTrip,
   updateTripStatus,
   assignDriverToTrip,
-  getRoute
+  getRoute,
 };
