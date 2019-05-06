329e1a8ef0a63b5d75242d512068e8a622e54e6e
Julián Scialabba <julian.scialabba@gmail.com>
2019-05-06 15:34:56 -0300

    Change cost distance

--- eukanuber-backend/src/services/tripsService.ts
+++ eukanuber-backend/src/services/tripsService.ts
@@ -130,7 +130,7 @@ async function calculateTripCost(distance: number, pets: string[], activeDrivers
   const driversExtraCost = activeDrivers > 0.5 ? 0 : 50;
 
   // Hay un costo por distancia (a mayor distancia, más caro)
-  const distanceCost = distance * 0.2;
+  const distanceCost = distance * 0.05;
 
   return petsExtraCost + timeExtraCost + driversExtraCost + distanceCost;
 }
