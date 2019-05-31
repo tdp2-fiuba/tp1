ab546905084671f5adb65cb94be0389d27cbd629
Maria Mariotti <mmariotti@mmariotti-ltm.internal.salesforce.com>
2019-05-31 00:40:54 -0300

    Moved integration test scripts.

---
+++ eukanuber-backend/integration-test-scripts/data.py
@@ -0,0 +1,128 @@
+positions = {
+    "-34.5616051,-58.455745": "Av. Cabildo y Av. Juramento, Buenos Aires, Argentina",
+    "-34.6006551,-58.3906953": "Av.Cordoba y Talcahuano, Buenos Aires, Argentina",
+    "-34.5903544,-58.4072665": "Av. Cnel Diaz y Av. Santa fe, Buenos Aires, Argentina",
+    "-34.5921745,-58.4059315": "Av. Santa fe y pueyrredon, Buenos Aires, Argentina",
+    "-34.5837348,-58.4137576": "Av. Las Heras y Scalabrini Ortiz, Buenos Aires, Argentina"
+}
+
+coordinates = {
+    "Av. Cabildo y Av. Juramento, Buenos Aires, Argentina": "-34.5616051,-58.455745",
+    "Av.Cordoba y Talcahuano, Buenos Aires, Argentina": "-34.6006551,-58.3906953",
+    "Av. Cnel Diaz y Av. Santa fe, Buenos Aires, Argentina": "-34.5903544,-58.4072665",
+    "Av. Santa fe y pueyrredon, Buenos Aires, Argentina": "-34.5921745,-58.4059315",
+    "Av. Las Heras y Scalabrini Ortiz, Buenos Aires, Argentina": "-34.5837348,-58.4137576"
+}
+
+
+driver1 = {
+    "firstName": "Agatha",
+    "lastName": "Christie",
+    "userType":"driver",
+    "position": coordinates["Av. Cabildo y Av. Juramento, Buenos Aires, Argentina"],
+    "fbId":"103078040909960",
+    "fbAccessToken": "EAAIhLc7Sm4wBANBZB5CTTv55WGipytlnqQ6vEgNdrxgNU7npelj3YrZAMNZA535rao44HklmmhKf9IZCca3gCleftqcv75FGBUFgjPocJLjtFNiOKbeu2HYmRDqzR8ZAS8lafzZCw0xTKEB7UBto9cKHuGbndFYZBS8k8EzRotVZBCDZCciaAZCFyZBZCgC4bdwBVZATGQ1sDJhgJVwZDZD",
+    "images": [{
+            "fileName":"vehicle",
+            "fileContent": "iVBORw0KGgoAAAW9DqM66GglqxXLnop0Uy"
+        }],
+        "car":{
+            "brand":"audi",
+            "model":"A3",
+            "plateNumber":"AAA123"
+        }
+}
+
+driver2 = {
+    "firstName": "J.K",
+    "lastName": "Rowling",
+    "userType":"driver",
+    "position": coordinates["Av.Cordoba y Talcahuano, Buenos Aires, Argentina"],
+    "fbId":"110281216872869",
+    "fbAccessToken": "EAAIhLc7Sm4wBAANlut4EuJtOF43MTQHX2DPjgMa7C07xQrZCCmZCZABuBG0ICPnejIIuXYj5fH40VcioJAahEXwOWpRBl1Xxz1ZB9F3o7TUGZBQpje6HikW2dxaHE9U8wFBAcEZAYs0z5Sn7OcdnD6p6jqtbaD4UJrqTeL6JQSMZC3dKFJqubreUcboIgle0lkMXWLVHNh0fxXE8sKVQpPu",
+    "images": [
+        {
+            "fileName":"vehicle",
+            "fileContent": "iVBORw0KGgoAAAW9DqM66GglqxXLnop0Uy"
+        }
+        ],
+        "car":{
+            "brand":"audi",
+            "model":"A3",
+            "plateNumber":"AAA123"
+        }
+}
+
+driver3 = {
+    "firstName": "Sherlock",
+    "lastName": "Holmes",
+    "userType":"driver",
+    "position": coordinates["Av. Cnel Diaz y Av. Santa fe, Buenos Aires, Argentina"],
+    "fbId":"107261353845723",
+    "fbAccessToken": "EAAIhLc7Sm4wBAET3hjSsxQ4YjNw8Y5hflZBbZBTEAQWMIL2JXXXkS68ZBtTb7Lx5xXN0bTBKHlS2TSi8opIOFAl7Tzmki4Bmu8wgb9dKCF0AwKme6ceGf0RsqH8bAOB1AVuQLMpVZC3AcIo4XFhWp5fu6CEfqWCjkgCfmwOG2nwA3AQ76ZASN4MnqaZAdnyp8I6aDXlJaUt2GBqCkY3OZBy",
+    "images": [
+        {
+            "fileName":"vehicle",
+            "fileContent": "iVBORw0KGgoAAAW9DqM66GglqxXLnop0Uy"
+        }
+        ],
+        "car":{
+            "brand":"audi",
+            "model":"A3",
+            "plateNumber":"AAA123"
+        }
+}
+
+passenger1 = {
+    "firstName": "H.P",
+    "lastName": "Lovecraft",
+    "userType":"passenger",
+    "position": coordinates["Av. Las Heras y Scalabrini Ortiz, Buenos Aires, Argentina"],
+    "fbId":"101327367780216",
+    "fbAccessToken": "EAAIhLc7Sm4wBAJIXsNvTigZAWqk7IYahgUQLR1xhpnUmYKpSc1YuA4tO77jItNq9Y9nuRKoIAkL39ZAHY94TAJMKrlw2Hy8jbDdlkmLB1e7JlxCOokbmlA2OH8CByWdnZAw2YwTHgbhQIh05v1z4u2cjwr9g57vDXbUEIfwFM7NHUGLtwJQVieXot79s0LZCHZBgpglaks9D8wV1xl9ZB5",
+    "images": [
+        {
+            "fileName":"vehicle",
+            "fileContent": "iVBORw0KGgoAAAW9DqM66GglqxXLnop0Uy"
+        }
+        ]
+}
+
+passenger2 = {
+    "firstName": "Brandon",
+    "lastName": "Sanderson",
+    "userType":"passenger",
+    "position": "-34.5837348,-58.4137576",
+    "fbId":"102387814339666",
+    "fbAccessToken": "EAAIhLc7Sm4wBAMY6bcPJHUammH63ZC8X52NZB0JoZBvgPEUXAXORHkkn0UaTR8LZBoX8SN30kKDKfnRe2eT7ACmueMZB5kiyIgceak6b1n7w701Kuvlp2A38m2ZAlz1ePFvnx42OFDvqL1L2W8Ot0UDRbQKWV34kkkwH0CrMaw9dHxianTkqejHvhcFWxCbHZAt3oYO8TmfoO3wEX8yLjes",
+    "images": [
+        {
+            "fileName":"vehicle",
+            "fileContent": "iVBORw0KGgoAAAW9DqM66GglqxXLnop0Uy"
+        }
+        ]
+}
+
+trip1 = {
+    "origin": positions[passenger1["position"]],
+    "destination": "Avenida Paseo Colón 850, Buenos Aires, Argentina",
+    "originCoordinates": passenger1["position"],
+    "destinationCoordinates": "",
+    "clientId": "81c63649-98c9-4b59-8a7a-14499ce0c6e0",
+    "driverId": "",
+    "pets": ["S", "M", "L"],
+    "status": "2",
+    "payment": "cash",
+    "price": "100 USD",
+    "routes": ""
+  }
+
+
+review = {
+    "userId":"32652240-bd54-42d4-8ab3-00c5173a8e99", 
+    "tripId":"5ba33b8a-dfd6-4e43-90c7-317e79fd95c9",
+    "review": {
+        "stars": 5,
+        "comment": "this was an awesome trip!"
+    }
+}
