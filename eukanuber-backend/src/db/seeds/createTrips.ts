57a1e4a4bb0c2b1b11e8972d139881eeb579f816
Nano Vazquez <marianodvazquez@gmail.com>
2019-06-03 02:13:40 -0300

    [backend] Update seeds

--- eukanuber-backend/src/db/seeds/createTrips.ts
+++ eukanuber-backend/src/db/seeds/createTrips.ts
@@ -1,37 +1,41 @@
-import Knex from 'knex';
+import Knex from "knex";
 
 async function seed(knex: Knex) {
-    const providersQueryBuilder = knex('trips');
+  const providersQueryBuilder = knex("trips");
 
-    const newTrip1 = {
-        origin: 'Goya 630, Buenos Aires, Argentina',
-        destination: 'Avenida Paseo Colón 850, Buenos Aires, Argentina',
-        originCoordinates: '43.138092,-89.747988',
-        destinationCoordinates: '43.017218,-89.831479',
-        clientId: '31bf9cab-d665-4553-8edd-8d2c2ca8f0e3',
-        driverId: '31bf9cab-d665-4553-8edd-8d2c2ca8f0e4',
-        pets: 'S, M, L',
-        status: 4,
-        payment: 'cash',
-        price: '100 USD'
-    };
-    const newTrip2 = {
-        origin: 'Goya 631, Buenos Aires, Argentina',
-        destination: 'Avenida Paseo Colón 851, Buenos Aires, Argentina',
-        originCoordinates: '43.138092,-89.747988',
-        destinationCoordinates: '43.017218,-89.831479',
-        clientId: '31bf9cab-d665-4553-8edd-8d2c2ca8f0e3',
-        driverId: '31bf9cab-d665-4553-8edd-8d2c2ca8f0e4',
-        pets: 'S, M, L',
-        status: 4,
-        payment: 'cash',
-        price: '100 USD'
-    };
-    // Deletes ALL existing entries
-    await providersQueryBuilder.del();
+  const newTrip1 = {
+    createdDate: new Date(),
+    origin: "Goya 630, Buenos Aires, Argentina",
+    destination: "Avenida Paseo Colón 850, Buenos Aires, Argentina",
+    originCoordinates: "43.138092,-89.747988",
+    destinationCoordinates: "43.017218,-89.831479",
+    clientId: "31bf9cab-d665-4553-8edd-8d2c2ca8f0e3",
+    driverId: "51bf9cab-d665-4553-8edd-8d2c2ca8f0e4",
+    pets: "S, M, L",
+    status: 4,
+    payment: "cash",
+    price: "$300"
+  };
 
-    // Inserts new entries
-    return providersQueryBuilder.insert([newTrip1, newTrip2]);
+  const newTrip2 = {
+    createdDate: new Date(),
+    origin: "Avenida Paseo Colón 851, Buenos Aires, Argentina",
+    destination: "Goya 631, Buenos Aires, Argentina",
+    originCoordinates: "43.138092,-89.747988",
+    destinationCoordinates: "43.017218,-89.831479",
+    clientId: "31bf9cab-d665-4553-8edd-8d2c2ca8f0e3",
+    driverId: "51bf9cab-d665-4553-8edd-8d2c2ca8f0e4",
+    pets: "S, M, L",
+    status: 4,
+    payment: "cash",
+    price: "$350"
+  };
+
+  // Deletes ALL existing entries
+  await providersQueryBuilder.del();
+
+  // Inserts new entries
+  return providersQueryBuilder.insert([newTrip1, newTrip2]);
 }
 
-export {seed};
+export { seed };
--- eukanuber-backend/src/db/seeds/createUsers.ts
+++ eukanuber-backend/src/db/seeds/createUsers.ts
@@ -6,34 +6,49 @@ async function seed(knex: Knex) {
   const newUser = {
     id: "31bf9cab-d665-4553-8edd-8d2c2ca8f0e3",
     userType: "client",
-    fbId: "1234",
-    fbAccessToken: "12345",
+    fbId: "1",
+    fbAccessToken: "11111",
     firstName: "Alejandro",
     lastName: "Fantino",
     access: 0,
     state: 0,
-    latitude: "12345",
-    longitude: "12345"
+    latitude: "-34.778050",
+    longitude: "-58.288390"
   };
+
   const newUser2 = {
-    id: "31bf9cab-d665-4553-8edd-8d2c2ca8f0e4",
+    id: "41bf9cab-d665-4553-8edd-8d2c2ca8f0e4",
     userType: "client",
     firstName: "Lionel",
-    fbId: "12345",
-    fbAccessToken: "12345",
     lastName: "Messi",
+    fbId: "2",
+    fbAccessToken: "22222",
     access: 0,
     state: 0,
     loggedIn: true,
-    latitude: "12345",
-    longitude: "12345"
+    latitude: "-34.600040",
+    longitude: "-58.511570"
+  };
+
+  const newUser3 = {
+    id: "51bf9cab-d665-4553-8edd-8d2c2ca8f0e4",
+    userType: "driver",
+    firstName: "Javier",
+    lastName: "Mascherano",
+    fbId: "3",
+    fbAccessToken: "33333",
+    access: 0,
+    state: 0,
+    loggedIn: true,
+    latitude: "-34.616610",
+    longitude: "-58.444760"
   };
 
   // Deletes ALL existing entries
   await providersQueryBuilder.del();
 
   // Inserts new entries
-  return providersQueryBuilder.insert([newUser, newUser2]);
+  return providersQueryBuilder.insert([newUser, newUser2, newUser3]);
 }
 
 export { seed };
